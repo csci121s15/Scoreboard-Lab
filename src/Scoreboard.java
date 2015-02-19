92d3884578cfa2c7dd94bd89105433e57627b757
Dan Howard <danhoward1994@gmail.com>
2015-02-19 15:58:17 -0500

    i

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -5,20 +5,40 @@ public class Scoreboard {
   private int maxCount;
   
   public Scoreboard(int maxScoreCount) {
-    scores = new ArrayList<>(maxScoreCount);
+    scores = new ArrayList<Integer>(maxScoreCount);
     maxCount = maxScoreCount;
   }
   
   public void addScore(int score) {
+    if (!isHighScore(score)){
+      return;
+    }
+    if (scores.size() < maxCount){
+      scores.add(score);
+    }
     scores.add(score);
+    
+    for (int i = scores.size() - 1; i > 0; i--){
+      if (scores.get(i) > scores.get(i - 1)){
+        int temp = scores.get(i);
+        scores.set(i, scores.get(i - 1));
+        scores.set(i - 1, temp);        
+    }
   }
+}
   
   public boolean isHighScore(int score) {
     
+    if (scores.size() == 0 ||  scores.size() ){
     return true;
   }
+    else {
+      return false;
+  }
+}
   
   public int getScore(int position) {
+   
     if (position < scores.size()) {
       return scores.get(position);
     }
