caf8922af42e605de509096c8a497785ef3de88e
John Ferguson <stjpf01@moravian.edu>
2015-02-19 15:55:13 -0500

    John Ferguson Scoreboard Lab Done

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -5,17 +5,43 @@ public class Scoreboard {
   private int maxCount;
   
   public Scoreboard(int maxScoreCount) {
-    scores = new ArrayList<>(maxScoreCount);
+    scores = new ArrayList<Integer>(maxScoreCount);
     maxCount = maxScoreCount;
   }
   
   public void addScore(int score) {
-    scores.add(score);
+    if (!isHighScore(score)) {
+      return;
+    }
+    
+    if (scores.size() < maxCount) {
+      scores.add(score);
+    }
+    else {
+     scores.set(scores.size() - 1, score); 
+    }
+    
+ 
+    for (int i = scores.size() - 1; i > 0; i--) {
+    if (scores.get(i) > scores.get(i-1)) {
+      int temp = scores.get(i-1);
+      scores.set((i-1), scores.get(i));
+      scores.set(i, temp);
+    }
+  }
   }
   
   public boolean isHighScore(int score) {
+    if (scores.size() == 0 || scores.size() < maxCount) {
+      return true;
+    }
+    else if (score > scores.get(scores.size() - 1)) {
+      return true;
+    }
+    else {
+      return false;
+    }
     
-    return true;
   }
   
   public int getScore(int position) {
