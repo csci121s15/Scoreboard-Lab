daba6a383d8ba385b4d7bcf766e02687a3b603cb
Jacob Fritz <stjrf05@moravian.edu>
2015-02-19 16:36:04 -0500

    Submission for the Scoreboard lab

---
+++ FritzAnswers.md
@@ -0,0 +1,7 @@
+**Question 1:** In terms of the mantra, describe the tests you need to verify the correctness of a non-full Scoreboard with more than two entries. Be specific about how many tests you need.
+	You only need two tests. You need a to test to confirm that the numbers are in the correct order and a test to confirm the size of the scoreboard.
+	(*Note* All can be done in one Test Method)
+	
+**Question 2:** In terms of the mantra, describe the tests you need to verify the corrections of a full Scoreboard. Be speicific about how many tests you need. HINT: Think carefully about the "bottom" of the Scoreboard.
+	You need three tests. One to confirm the numbers are in the right order, one to confirm the size of the scoreboard, and one to confirm the minimum number for a high score.
+	(*Note* All can be done in one Test Method)
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -5,17 +5,45 @@ public class Scoreboard {
   private int maxCount;
   
   public Scoreboard(int maxScoreCount) {
-    scores = new ArrayList<>(maxScoreCount);
+    scores = new ArrayList<Integer>(maxScoreCount);
     maxCount = maxScoreCount;
   }
   
-  public void addScore(int score) {
-    scores.add(score);
+  public void addScore(int score) {    
+    if(isHighScore(score))
+    {
+      for(int i=0; i<scores.size();i++)
+      {
+        if(score > scores.get(i))
+        {
+          scores.add(i,score);
+          if(scores.size() > maxCount)
+          {
+            for(int j=maxCount; j<scores.size();j++)
+              scores.remove(j);
+          }
+          return;
+        }
+      }
+      if(scores.size() < maxCount)
+        scores.add(score);
+      if(scores.size() > maxCount)
+      {
+        for(int k=maxCount; k<scores.size();k++)
+          scores.remove(k);
+      }
+    }
   }
   
   public boolean isHighScore(int score) {
-    
-    return true;
+    for(int i=0; i<=maxCount-1; i++)
+    {
+      if(scores.size() < maxCount)
+        return true;
+      if(score >= scores.get(i))
+        return true;
+    }
+    return false;
   }
   
   public int getScore(int position) {
