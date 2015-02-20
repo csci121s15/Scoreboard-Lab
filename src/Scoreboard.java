8ada604d075975b12a0fd17a961b2cb4cd73561d
Anna Lamoureux <anna.lamoureux@gmail.com>
2015-02-20 01:37:15 -0500

    Answers, Scoreboard and its testing class

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -5,17 +5,24 @@ public class Scoreboard {
   private int maxCount;
   
   public Scoreboard(int maxScoreCount) {
-    scores = new ArrayList<>(maxScoreCount);
+    scores = new ArrayList<Integer>(maxScoreCount);
     maxCount = maxScoreCount;
   }
   
   public void addScore(int score) {
     scores.add(score);
+    
+    for(int i = scores.size() - 1; i >= 0; i--) {
+      if(score > scores.get(i)) {
+        scores.set(i+1, scores.get(i));
+        scores.set(i, score);
+      }
+    }
   }
   
   public boolean isHighScore(int score) {
     
-    return true;
+      return true;
   }
   
   public int getScore(int position) {
--- src/ScoreboardTest.java
+++ src/ScoreboardTest.java
@@ -18,4 +18,80 @@ public class ScoreboardTest extends TestCase {
     assertEquals(50, sb.getScore(0));
     assertEquals(1, sb.getNumScores());
   }
+  
+  public void testSecondScoreBeforeFirst() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(50);
+    sb.addScore(25);
+    
+    assertTrue(sb.isHighScore(10));
+    assertEquals(50, sb.getScore(0));
+    assertEquals(25, sb.getScore(1));
+    assertEquals(2, sb.getNumScores());
+  }
+  
+  public void testSecondScoreAfterFirst() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(25);
+    sb.addScore(50);
+    
+    assertTrue(sb.isHighScore(10));
+    assertEquals(50, sb.getScore(0));
+    assertEquals(25, sb.getScore(1));
+    assertEquals(2, sb.getNumScores());
+  }
+  
+  public void testThreeScores() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(25);
+    sb.addScore(50);
+    sb.addScore(32);
+    
+    assertTrue(sb.isHighScore(10));
+    assertEquals(50, sb.getScore(0));
+    assertEquals(25, sb.getScore(2));
+    assertEquals(32, sb.getScore(1));
+    assertEquals(3, sb.getNumScores());
+  }
+  
+  public void testFullScoreboardWithHighScore() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(25);
+    sb.addScore(50);
+    sb.addScore(32);
+    sb.addScore(45);
+    sb.addScore(14);
+    sb.addScore(64);
+    sb.addScore(87);
+    sb.addScore(35);
+    sb.addScore(90);
+    sb.addScore(39);
+    sb.addScore(71);
+    sb.addScore(38);
+    
+    assertEquals(25, sb.getScore(10));
+  }
+  
+  public void testFullScoreboardWithLowScore() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(25);
+    sb.addScore(50);
+    sb.addScore(32);
+    sb.addScore(45);
+    sb.addScore(14);
+    sb.addScore(64);
+    sb.addScore(87);
+    sb.addScore(35);
+    sb.addScore(90);
+    sb.addScore(39);
+    sb.addScore(27);
+    sb.addScore(8);
+
+    assertEquals(14, sb.getScore(10));
+  }
 }
