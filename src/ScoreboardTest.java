77d4abe3044c4ffd364e448e1774d62f30403e3e
Marisa Pacitti <stmtp05@moravian.edu>
2015-02-26 14:25:33 -0500

    Scoreboard Lab

--- src/ScoreboardTest.java
+++ src/ScoreboardTest.java
@@ -18,4 +18,58 @@ public class ScoreboardTest extends TestCase {
     assertEquals(50, sb.getScore(0));
     assertEquals(1, sb.getNumScores());
   }
+  
+  public void testSecondScoreBeforeFirst() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(50);
+    sb.addScore(51);
+    
+    assertTrue(sb.isHighScore(10));
+    assertEquals(50, sb.getScore(1));
+    assertEquals(51, sb.getScore(0));
+    assertEquals(2, sb.getNumScores());
+  }
+    
+  
+  public void testSecondScoreAfterFirst() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(50);
+    sb.addScore(49);
+    
+    assertTrue(sb.isHighScore(10));
+    assertEquals(50, sb.getScore(0));
+    assertEquals(49, sb.getScore(1));
+    assertEquals(2, sb.getNumScores());
+  }
+  
+  public void testMulipleScoresNonFull() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(50);
+    sb.addScore(51);
+    sb.addScore(52);
+    
+    assertTrue(sb.isHighScore(10));
+    assertEquals(50, sb.getScore(2));
+    assertEquals(51, sb.getScore(1));
+    assertEquals(52, sb.getScore(0));
+    assertEquals(3, sb.getNumScores());
+  } 
+  
+  public void testMulipleScoresFull() {
+    Scoreboard sb = new Scoreboard(10);
+    
+    sb.addScore(50);
+    sb.addScore(51);
+    sb.addScore(52);
+    sb.addScore(53);
+    
+    assertTrue(sb.isHighScore(10));
+    assertEquals(53, sb.getScore(0));
+    assertEquals(51, sb.getScore(2));
+    assertEquals(52, sb.getScore(1));
+    assertEquals(3, sb.getNumScores());
+}
 }
